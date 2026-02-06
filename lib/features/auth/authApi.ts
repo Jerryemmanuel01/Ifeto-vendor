8950f52cf00ae2b743f7649583c45ae4964c52f2
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2026-02-06 15:01:58 +0100

    feat: Implement user authentication flow including login, email verification, and related components, hooks, and API integrations.

--- lib/features/auth/authApi.ts
+++ lib/features/auth/authApi.ts
@@ -9,7 +9,33 @@ export const authApi = apiSlice.injectEndpoints({
         body: credentials,
       }),
     }),
+    verifyAuthCode: builder.mutation<any, { code: string; userId: string }>({
+      query: (data) => ({
+        url: "/auth/vendor/verify-email",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    resendVerificationCode: builder.mutation<any, { email: string }>({
+      query: (data) => ({
+        url: "/auth/vendor/resend-verification-code",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    login: builder.mutation<any, any>({
+      query: (credentials) => ({
+        url: "/auth/vendor/login",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
   }),
 });
 
-export const { useSignupMutation } = authApi;
+export const {
+  useSignupMutation,
+  useVerifyAuthCodeMutation,
+  useResendVerificationCodeMutation,
+  useLoginMutation,
+} = authApi;
